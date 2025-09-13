651eb5f4abd77ecc4aa77a8cf8a5678180edf709
Wesley0010012 <wglaurindo33@gmail.com>
2025-09-12 21:22:30 -0300

    feat: add post-creation script

---
+++ database/ddl/post-creation.sql
@@ -0,0 +1,9 @@
+---------------------------------------
+-- SCRIPT DE PRÉ-CRIAÇÃO DE ESTRUTURAS
+-- Este script executa a criação de pós-regras das relações do minimundo
+---------------------------------------
+-- Inclui constraints
+@/opt/scripts/ddl/constraints.sql
+
+-- Inclui constraints historiamento
+@/opt/scripts/ddl/history-constraints.sql
